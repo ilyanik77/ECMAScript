df77a977beb25b249ba0be0b50e0eb12f97d74df
ilya <ilyakhlamov77@gmail.com>
2023-08-06 11:29:26 +0300

    made changes

--- HW_1/script.js
+++ HW_1/script.js
@@ -6,3 +6,23 @@ console.log(getMinNumber([1, 5, 7, 9]));
 
 // 2) Напишите функцию createCounter, которая создает счетчик и возвращает объект с двумя методами: increment и decrement. Метод increment должен увеличивать значение счетчика на 1, а метод decrement должен уменьшать значение счетчика на 1. Значение счетчика должно быть доступно только через методы объекта, а не напрямую.
 
+const createCounter = (number) => {
+    return {
+        increment() {
+            console.log(++number); 
+        },
+
+        decrement() {
+            console.log(--number);
+        },
+    };
+};
+
+const counter = createCounter (10);
+counter.increment();
+counter.increment();
+counter.increment();
+
+counter.decrement();
+counter.decrement();
+counter.decrement();
