42cbd22d1d1c5a7186670ac0470a75f204a2875f
ilya <ilyakhlamov77@gmail.com>
2023-08-06 11:36:46 +0300

    made changes

--- HW_1/script.js
+++ HW_1/script.js
@@ -29,3 +29,19 @@ counter.increment();
 counter.decrement();
 counter.decrement();
 counter.decrement();
+
+// 3) Напишем функцию, которая будет находить факториал числа с использованием рекурсии:
+// примеры вызова функции
+// console.log(factorial(5)); // выводит 120 (5 * 4 * 3 * 2 * 1)
+// console.log(factorial(0)); // выводит 1 (по определению факториала)
+
+const factorial = (number) => {
+    if (number == 0 || number == 1) return 1;
+     else {
+        return factorial(number - 1) * number;
+    }
+};
+
+console.log(factorial(0));
+console.log(factorial(5));
+console.log(factorial(1));
